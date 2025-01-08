72df1db5ae2b3443367b4c4a734117cdf8f3d7e5
Jakub Praszkowski <kubapraszkowski@gmail.com>
2025-01-08 22:10:06 +0100

    Added Role based auth

--- src/main/java/com/kubuski/urlshortener/controller/UserController.java
+++ src/main/java/com/kubuski/urlshortener/controller/UserController.java
@@ -5,17 +5,19 @@ import com.kubuski.urlshortener.dto.UserResponse;
 import com.kubuski.urlshortener.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/users")
-final class UserController {
+class UserController {
 
     private final UserService userService;
 
     @GetMapping("/{email}")
     @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasRole('ADMIN')")
     public UserResponse getUserByEmail(@PathVariable String email) {
         return userService.findByEmail(email);
     }
